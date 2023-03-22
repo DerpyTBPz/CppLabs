b0e42db54490fa0d77a06a83427c3150451c2232
DerpyTBPz <dima.plyasov13@gmail.com>
2023-03-22 12:21:26 +0200

    task3

--- task3/main.cpp
+++ task3/main.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
 
 int main()
-{   
-    int n;
+{
+    int n, k = 0;
+    std::cout << "Enter array size:";
     std::cin >> n;
-
-    if(n > 10)
+    
+    if((n > 10) || (n <= 0))
     {
-        std::cout << "Wrong array size";
+        std::cout << "\nWrong array size\n";
     }
     else
     {
+        std::cout << "\nSolution:\n";
         int *A = new int[n];
         int *B = new int[n];
-
+        
         for(int i = 0; i < n; i++)
         {
             std::cin >> A[i];
         }
-
+        
         for(int i = 0; i < n; i++)
         {
             if((A[i] % 5 == 0) || (A[i] % 3 == 0))
             {
-                B[i] = i;
+                B[k] = i;
+                k++;
             }
         }
-
-        for(int i = 0; i < n; i++)
+        
+        for(int i = 0; i < k; i++)
         {
             std::cout << B[i] << " - " << A[B[i]] << "\n";
         }
-    }   
-
+    }
     return 0;
 }
