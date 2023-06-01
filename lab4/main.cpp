d58e78af67bc73902bf87cdba9ea3adc80ff4cd5
derpytbpz <dima.plyasov13@gmail.com>
2023-06-01 14:39:47 +0300

    lab4

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -57,31 +57,3 @@ int main()
 
     return 0;
 }
-
-
-// #include <iostream>
-// #include "array.h"
-
-
-// int main()
-// {      
-//     Array arr1, arr2;
-//     arr1.setValue(6);
-//     arr1.setValue(60);
-//     arr2.setValue(40);
-//     arr2.setValue(40);
-
-//     if( arr1 == arr2) 
-//     {
-//         std::cout << "Eq" <<std::endl;
-//     }
-
-//     if( arr1 != arr2) 
-//     {
-//         std::cout << "Not Eq" <<std::endl;
-//     }
-
-//     Array arr3 = arr1 + arr2;
-
-//     return 0;
-// }
